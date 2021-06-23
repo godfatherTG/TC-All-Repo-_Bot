47d94ebecca7004a1cb6303f6bbbe05fe0746ffd
GODFATHER <85017302+godfatherTG@users.noreply.github.com>
2021-06-23 23:44:01 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,4 +1,32 @@
-start_mess
+class script(object):
 
-          hey <\b> iam repo bot join our channel 
-@TC_BOTZ
+
+    START_MSG = """ <b>Hi {}
+
+You can call this as an Auto Filter Bot if you like
+This bot is exclusively made for @Cmkmedia
+
+You can see me in @Cmkmedia <i>help</i></b>"""
+
+
+    HELP_MSG = """<b>How to use the bot??</b>
+<i>    Sorry you can't use me 😔,for movies join @CTMTMT
+</i>
+
+<b>© @CTMTMT</b>"""
+
+
+    ABOUT_MSG = """⭕️<b>My Name :BOTZ</b>
+    
+⭕️<b>Creater :</b> @TC_BOTZ
+
+⭕️<b>Language :</b> <code>Python3</code>
+
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+
+⭕️<b>support group :</b> <a href='https://t.me/Cmkmedia'</a> 
+
+
+⭕<b>owner :</b> <a href='https://t.me/CmkMediaadmin'</a> 
+
+
