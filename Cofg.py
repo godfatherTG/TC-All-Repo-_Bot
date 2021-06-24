8922ac5bc4dd6951c26e54ef99b9025eb7b7533f
GODFATHER <85017302+godfatherTG@users.noreply.github.com>
2021-06-24 09:32:06 +0530

    Update Cofg.py

--- Cofg.py
+++ Cofg.py
@@ -1,65 +1,43 @@
-import os
-import logging
-from logging.handlers import RotatingFileHandler
-
-#Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-
-#Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
-
-#Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
-
-#Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
-
-#OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-
-#force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
-
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
-
-#start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
-try:
-    ADMINS=[]
-    for x in (os.environ.get("ADMINS", "").split()):
-        ADMINS.append(int(x))
-except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
-
-#set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
-
-#Set true if you want Disable your Channel Posts Share button
-if os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True':
-    DISABLE_CHANNEL_BUTTON = True
-else:
-    DISABLE_CHANNEL_BUTTON = False
-
-ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
-
-LOG_FILE_NAME = "filesharingbot.txt"
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
-    datefmt='%d-%b-%y %H:%M:%S',
-    handlers=[
-        RotatingFileHandler(
-            LOG_FILE_NAME,
-            maxBytes=50000000,
-            backupCount=10
-        ),
-        logging.StreamHandler()
-    ]
-)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
+    import sys
+    print("The README is there to be read. Extend this sample config to a config file, don't just rename and change "
+          "values here. Doing that WILL backfire on you.\nBot quitting.", file=sys.stderr)
+    quit(1)
 
 
-def LOGGER(name: str) -> logging.Logger:
-    return logging.getLogger(name)
+# Create a new config.py file in same dir and import, then extend this class.
+class Config(object):
+    LOGGER = True
+
+    # REQUIRED
+    API_KEY = ""
+    OWNER_ID = "683538773" # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "ViruZs"
+
+    # RECOMMENDED
+    SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
+    MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
+    LOAD = []
+    NO_LOAD = ['translation', 'rss']
+    WEBHOOK = False
+    URL = None
+
+    # OPTIONAL
+    SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
+    SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    DONATION_LINK = None  # EG, paypal
+    CERT_PATH = None
+    PORT = 5000
+    DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
+    STRICT_GBAN = False
+    WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
+    BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
+    ALLOW_EXCL = False  # Allow ! commands as well as /
+
+
+class Production(Config):
+    LOGGER = False
+
+
+class Development(Config):
+    LOGGER = True
